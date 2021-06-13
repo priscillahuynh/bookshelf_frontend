3b64e54953eaa7eed8d8e674a2272eaab1db4d6e
priscillahuynh <priscillamhuynh@gmail.com>
2021-06-13 11:38:21 +0400

    install packages and import packages to index.js

--- src/App.js
+++ src/App.js
@@ -2,6 +2,13 @@ import './App.css';
 import React from 'react'
 
 class App extends React.Component {
+
+  componentDidMount() {
+    fetch('http://localhost:3000/api/v1/shelves')
+    .then(r => r.json()) 
+    .then(data => console.log(data))
+  }
+
   render() {
     return (
       <div className="App">
