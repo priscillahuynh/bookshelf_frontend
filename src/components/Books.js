bf75574ae8806e012aa8a9b3a746e3ce7887dc27
priscillahuynh <priscillamhuynh@gmail.com>
2021-06-16 11:19:31 +0400

    iterate through props to render each individual book title and image_url

--- src/components/Books.js
+++ src/components/Books.js
@@ -1,10 +1,11 @@
 import React from 'react';
 
 const Books = (props) => {
-    console.log(props)
     return (
         <div>
-            books
+            {props.books && props.books.map(book => 
+            <p key={book.id}><img src={book.image_url}/><br/>{book.title} </p>
+            )}
         </div>
     )
 } 
