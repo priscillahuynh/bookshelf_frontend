5d08cc476f27703ae20c22c55096fb353bdbf1f7
priscillahuynh <priscillamhuynh@gmail.com>
2021-06-17 11:42:18 +0400

    replace <p> tag with <div>

--- src/components/Shelf.js
+++ src/components/Shelf.js
@@ -2,16 +2,17 @@ import React from 'react';
 import BooksContainer from '../containers/BooksContainer';
 
 const Shelf =(props) => {
+    console.log(props)
     let shelf = props.shelves.filter(shelf => shelf.id == props.match.params.id)[0]
-    console.log(shelf)
+    
     return (
         <div>
             <h2>
                 {shelf ? shelf.name : null }
             </h2>
-                <p>
+                <div>
                 <BooksContainer shelf={shelf}/>
-                </p>
+                </div>
         </div>
     )
 }
