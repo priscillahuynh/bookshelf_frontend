30e82a66e81c17d2b34c41fcc277ad3960624985
priscillahuynh <priscillamhuynh@gmail.com>
2021-06-17 11:45:30 +0400

    add 'ADD_BOOK' case to reducer

--- src/reducers/shelfReducer.js
+++ src/reducers/shelfReducer.js
@@ -1,12 +1,22 @@
 
 export default function shelfReducer(state={shelves: []}, action) {
+    // debugger
     switch(action.type) {
         case 'FETCH_SHELVES':
             return {shelves: action.payload}
         case 'ADD_SHELF':
             return {...state, shelves:[...state.shelves, action.payload]}
+        case 'ADD_BOOK':
+            let shelves = state.shelves.map(shelf => {
+                if(shelf.id === action.payload.id) {
+                    return action.payload
+                } else {
+                    return shelf
+                }
+            })
+            return {...state, shelves: shelves}
         default: 
-        return state 
+            return state 
         
     }
 }
