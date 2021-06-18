4f897f1c44973f6cec496c45591ef8c2a2032be2
priscillahuynh <priscillamhuynh@gmail.com>
2021-06-18 06:01:51 +0400

    create addBook action

---
+++ src/actions/addBook.js
@@ -0,0 +1,13 @@
+export const addBook = (book, shelf_id) => {
+    return (dispatch) => {
+        fetch(`http://localhost:3000/api/v1/shelves/${shelf_id}/books`, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            'Accept': 'application/json'},
+        body: JSON.stringify(book)
+        })
+        .then(response => response.json())
+        .then(shelf => dispatch({type: 'ADD_BOOK', payload: shelf}))
+    }
+}
