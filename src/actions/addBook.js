5e14dc936351c83767ea10e0f0136112725c9172
priscillahuynh <priscillamhuynh@gmail.com>
2021-06-20 10:31:10 +0400

    properly format text in file

--- src/actions/addBook.js
+++ src/actions/addBook.js
@@ -1,13 +1,19 @@
 export const addBook = (book, shelf_id) => {
     return (dispatch) => {
         fetch(`http://localhost:3000/api/v1/shelves/${shelf_id}/books`, {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json',
-            'Accept': 'application/json'},
-        body: JSON.stringify(book)
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(book)
         })
-        .then(response => response.json())
-        .then(shelf => dispatch({type: 'ADD_BOOK', payload: shelf}))
-    }
+            .then(response => response.json())
+            .then(shelf => {
+                if (shelf.error) {
+                    alert(shelf.error)
+                } else {
+                    dispatch({ type: 'ADD_BOOK', payload: shelf })
+                }
+            }
+    )}
 }
