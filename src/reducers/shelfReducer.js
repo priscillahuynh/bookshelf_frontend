407ff4b4b4ea51015a4b6b17ef91c46ce904f492
priscillahuynh <priscillamhuynh@gmail.com>
2021-06-20 15:50:16 +0400

    refactor and combine return values for EDIT_SHELF, ADD_BOOK, and DELETE_BOOK. add DELETE_SHELF case

--- src/reducers/shelfReducer.js
+++ src/reducers/shelfReducer.js
@@ -1,12 +1,17 @@
 
 export default function shelfReducer(state={shelves: []}, action) {
-
+    
     switch(action.type) {
         case 'FETCH_SHELVES':
             return {shelves: action.payload}
-        case 'ADD_SHELF', 'DELETE_SHELF':
+        case 'ADD_SHELF':
             return {...state, shelves:[...state.shelves, action.payload]}
-        case 'EDIT_SHELF', 'ADD_BOOK', 'DELETE_BOOK':
+        case 'DELETE_SHELF':
+            return {...state, shelves: state.shelves.filter((shelf) => shelf.id !== action.payload.id)
+              }
+        case 'EDIT_SHELF': 
+        case 'ADD_BOOK':
+        case 'DELETE_BOOK':    
             let shelves = state.shelves.map(shelf => {
                 if(shelf.id === action.payload.id) {
                     return action.payload
